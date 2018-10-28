5fdf011dd5f3349738555c9d86c7e65c9ebdbd34
mattetress <mattetress@me.com>
2018-10-28 15:21:25 -0500

    edit dreams/index view to not show private dreams

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -32,7 +32,7 @@ class DreamsController < ApplicationController
   get "/dreams/:id" do
     @dream = Dream.find_by_id(params[:id])
     if logged_in? && !@dream.private || @dream.user == current_user
-      erb :show
+      erb :"dreams/show"
     else
       flash[:error] = "This dream is marked as private. Only the owner can view it."
       redirect "/dreams"
