206999adb4b1c0325512667e0a02709ae174fdf1
mattetress <mattetress@me.com>
2018-10-27 20:48:10 -0500

    add /dreams route

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,3 +1,12 @@
 class DreamsController < Sinatra::Base
 
+  get "/dreams" do
+    if logged_in?
+      erb :"dreams/index"
+    else
+      flash[:error] = "You must log in or sign up to view that page."
+      redirect "/"
+    end
+  end
+
 end
