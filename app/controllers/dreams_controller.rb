2ca00aed986b3c9d4165ad22417ed8e81c54b974
mattetress <mattetress@me.com>
2018-10-27 23:06:56 -0500

    create dreams/show route

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -9,4 +9,13 @@ class DreamsController < ApplicationController
     end
   end
 
+  get "/dreams/:id" do
+    @dream = Dream.find_by_id(params[:id])
+    if logged_in?
+      erb :"dreams/show"
+    else
+      flash[:error] = "You must log in or sign up to view that page."
+      redirect "/"
+    end
+  end 
 end
