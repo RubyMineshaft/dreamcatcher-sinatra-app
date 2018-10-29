bab010eee12be0e139d7caeab5adc7bab72f6f43
mattetress <mattetress@me.com>
2018-10-29 14:20:45 -0500

    add view and action to edit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  get "/comments/:id/edit" do
+    @comment = Comment.find_by_id(params[:id])
+
+    if logged_in? && @comment.user == current_user
+      erb :"comments/edit"
+    else
+      flash[:error] = "Only the owner of a comment can edit it."
+      redirect "/dreams/#{@comment.dream.id}"
+    end
+  end
+
 end
