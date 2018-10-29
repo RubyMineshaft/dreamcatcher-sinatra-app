1613deb9fc59f98c8a98bd677fbb29a4ae030ad5
mattetress <mattetress@me.com>
2018-10-29 14:32:19 -0500

    add controller action for new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
 
+  post "/comments" do
+    @comment = Comment.new(params[:comment])
+    @comment.save
+    current_user.comments << @comment
+    @comment.dream.comments << @comment
+
+    redirect "/dreams/#{@comment.dream.id}"
+  end
+
   get "/comments/:id/edit" do
     @comment = Comment.find_by_id(params[:id])
 
