f16a1b03d3cb47fc4a87caf098026b908b61849b
mattetress <mattetress@me.com>
2018-10-30 12:21:15 -0500

    create /users/:id route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
-end 
+
+  get "/users/:id" do
+    @user = User.find_by(params[:id])
+    if logged_in?
+      erb :"users/show"
+    else
+      redirect "/"
+    end 
+  end
+
+
+end
