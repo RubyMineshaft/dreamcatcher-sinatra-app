af55d9e0f8660489a293da18f9d7ecd520be0d18
mattetress <mattetress@me.com>
2018-10-30 21:20:47 -0500

    add show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
-end 
+
+  get "/categories/:id" do
+    @category = Category.find_by(id: params[:id])
+    if logged_in?
+      erb :"categories/show"
+    else
+      redirect "/"
+    end 
+  end
+
+end
