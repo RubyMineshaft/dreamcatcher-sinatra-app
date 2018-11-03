5a363bb29d682a7eef40e0a82e9b5aba8ea421f0
mattetress <mattetress@me.com>
2018-11-03 16:40:37 -0500

    move sign up, login, logout actions to users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,47 +13,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get "/signup" do
-    @user = User.new
-
-    if logged_in?
-      redirect to "/dreams"
-    end
-    erb :signup
-  end
-
-  post "/signup" do
-    @user = User.new(params[:user])
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to "/dreams"
-    else
-      erb :signup
-    end
-  end
-
-  get "/login" do
-    redirect to "/dreams" if logged_in?
-    erb :login
-  end
-
-  post "/login" do
-    @user = User.find_by(email: params[:user][:email])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect "/dreams"
-    else
-      flash[:error] = "There was a problem logging you in.  Please check email address and password."
-      redirect "/login"
-    end
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
-
   helpers do
     def logged_in?
       !!session[:user_id]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,46 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    @user = User.new
+
+    if logged_in?
+      redirect to "/dreams"
+    end
+    erb :signup
+  end
+
+  post "/signup" do
+    @user = User.new(params[:user])
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to "/dreams"
+    else
+      erb :signup
+    end
+  end
+
+  get "/login" do
+    redirect to "/dreams" if logged_in?
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect "/dreams"
+    else
+      flash[:error] = "There was a problem logging you in.  Please check email address and password."
+      redirect "/login"
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
   get "/users/:id" do
     @user = User.find_by(id: params[:id])
     if logged_in?
@@ -30,6 +71,4 @@ class UsersController < ApplicationController
       erb :"users/edit"
     end
   end
-
-
 end
